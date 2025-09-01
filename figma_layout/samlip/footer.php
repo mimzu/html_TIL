1b4cf24bcce2febb988f07f31e1305a6764c58f3
passminju <passminju@gmail.com>
2025-09-01 18:12:54 +0900

    반응형 웹 연습

---
+++ figma_layout/samlip/footer.php
@@ -0,0 +1,34 @@
+<footer>
+            <div class="ft_top">
+                <ul>
+                    <li><a href="#"><strong>개인정보 처리 방침</strong></a></li>
+                    <li><a href="#"><strong>영상정보처리기기 운영관리방침</strong></a></li>
+                    <li><a href="#">거래희망회사 사전등록</a></li>
+                    <li><a href="#">동반성장</a></li>
+                    <li><a href="#"><img src="./image/ccm_logo.png" alt="공정거래위원회 바로가기"></a></li>
+                </ul>
+                <div class="ft_select">
+                    <ul class="family_site">
+                        <li><a href="#">Family Sites</a></li>
+                        <li><ul class=family_list>
+                            <li vlaue="#">Paris Baguette</li>
+                            <li vlaue="#">Baskin Robbins</li>
+                            <li vlaue="#">Dunkin'</li>
+                            <li vlaue="#">Paris Croissant</li>
+                            <li vlaue="#">Shake Shack</li>
+                        </ul></li>
+                    </ul>
+                    <ul class="sitemap_wrap"><!-- 배경 색을 위한 div -->
+                        <li>SITE MAP</li>
+                        <li><div class="sitemap_list">
+                                <h2>Sitemap</h2>
+                                <button type="button" class="close">X</button>
+                                <!-- 사이트맵 목록 작성위치 -->
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <address>Customer Service  전화번호 080-739-8572 (월~금 09:00~17:00)<br>101, Gongdan 1-daero, Siheung-si, Gyeonggi-do, Republic of Korea ㈜SPC삼립 대표이사 황종현</address>
+            <p class="company_info">&copy; SPC Samlip. All rights reserved.</p>
+        </footer> 
